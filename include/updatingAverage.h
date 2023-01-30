ba67900b0f3ffd1fd6b61ee3e44f8f040d7421cd
Kilian Schulz <ge64zuw@mytum.de>
2023-01-30 10:12:00 +0100

    fixed bugs in updatingAverage.h where it got suck after the first push

--- include/updatingAverage.h
+++ include/updatingAverage.h
@@ -15,18 +15,20 @@ class UpdatingAverage {
     std::size_t maxSize, idx;
     T sum;
 public:
-    explicit UpdatingAverage(std::size_t size) : maxSize(size), idx(0), sum(0) {}
+    explicit UpdatingAverage(std::size_t size) : maxSize(size), idx(0), sum(0) {
+        entries.reserve(maxSize);
+    }
 
     void push(T el) {
         if (entries.size() < maxSize) {
             entries.push_back(el);
+            sum += entries[idx];
         } else {
             sum -= entries[idx];
             entries[idx] = el;
+            sum += entries[idx];
         }
-        sum += entries[idx];
-
-        idx = (idx + 1) % entries.size();
+        idx = (idx + 1) % maxSize;
     }
 
     T getAverage() {
@@ -34,7 +36,7 @@ public:
     }
 
     [[nodiscard]] std::size_t getNumEntries() const {
-        return entries.size();
+        return maxSize;
     }
 };
 
